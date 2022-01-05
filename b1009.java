30c16321f014262461981faea69806c4af5ea7f6
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-05 10:55:54 +0900

    Add b1009
    - 나머지 연산을 하여 결과를 찾아갔다

--- b1009.java
+++ b1009.java
@@ -1,3 +1,5 @@
+package com.donggi.gist;
+
 import java.util.Scanner;
 
 public class b1009 {
@@ -5,20 +7,18 @@ public class b1009 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        String[] stringNumberArray;
-
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int r = 1;
+            int res = 1;
 
             for (int j = 0; j < b; j++) {
-                r = a * r % 10;
-                if (r == 0) {
-                    r = 10;
+                res = a * res % 10;
+                if (res == 0) {
+                    res = 10;
                 }
             }
-            System.out.println(r);
+            System.out.println(res);
         }
     }
 }
